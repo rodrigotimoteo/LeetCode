46274965f2c48738a0ee92414f9bc05de048c68b
Rodrigo Timoteo <rodrigo.timoteo2603@gmail.com>
2023-12-02 17:02:44 +0000

    Solved 1160. Find Words that can be formed by Characters

---
+++ 1160/java_solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int count = 0;
+
+        for(String word : words) 
+            count += giveCharacters(word, chars);
+        
+        return count;
+    }
+
+    private int giveCharacters(String word, String chars) {
+        char[] wordChar = word.toCharArray();
+        char[] charChars = chars.toCharArray();
+
+        for(int i = 0; i < wordChar.length; i++) {
+            for(int l = 0; l < charChars.length; l++) {
+                if(wordChar[i] == charChars[l]) {
+                    wordChar[i] = '.';
+                    charChars[l] = '.';
+                    break;
+                }
+            }
+        }
+
+        for(char wordC : wordChar)
+            if(wordC != '.') return 0;
+
+        return wordChar.length;
+    }
+}

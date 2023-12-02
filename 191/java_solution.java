65155f17a0aff2dc397857711abfd5694cacea7a
Rodrigo Timoteo <rodrigo.timoteo2603@gmail.com>
2023-12-02 17:06:23 +0000

    fixed erros in file

--- 191/java_solution.java
+++ 191/java_solution.java
@@ -1,11 +1,12 @@
-public class Solution {
+class Solution {
 
     public int hammingWeight(int n) {
     
-    int count = 0;
+        int count = 0;
     
-    for(int i = 0; i < 32; i++) 
-        if((n & (0b1 << i)) == (0b1 << i)) count++;
+        for(int i = 0; i < 32; i++) 
+            if((n & (0b1 << i)) == (0b1 << i)) count++;
 
-    return count;
+        return count;
+    }
 }
